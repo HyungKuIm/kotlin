3274bf46538e05102f669671532a8f1406f4c406
A <damansa1@naver.com>
2024-10-04 15:01:16 +0900

    문자열 연습

---
+++ src/str_raw.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var msg = """
+        Kotlin은 JetBrains에서 개발한
+        객체지향 언어입니다.
+    """.trimIndent()
+    println(msg)
+}
---
+++ src/str_templ.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var value = 10
+    var data = arrayOf(100, 200, 300)
+    println("변수값은 ${value}")
+    println("배열의 마지막 값은 ${data[2]}")
+    println("배열의 크기는 ${data.size}")
+
+}
